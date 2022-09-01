56236719b8c33787d29a0fe126431053cbef8562
JoeyYoung <Cookie_8421@sina.cn>
2022-09-01 16:20:18 +0800

    Java SPI mechanism

---
+++ JavaBasis/src/main/java/Basis/SPI/Search.java
@@ -0,0 +1,7 @@
+package Basis.SPI;
+
+import java.util.List;
+
+public interface Search {
+    public List<String> searchDoc(String keyword);
+}
---
+++ JavaBasis/src/main/java/Basis/SPI/Test.java
@@ -0,0 +1,16 @@
+package Basis.SPI;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+public class Test {
+
+    public static void main(String[] args) {
+        ServiceLoader<Search> s = ServiceLoader.load(Search.class);
+        Iterator<Search> iterator = s.iterator();
+        while (iterator.hasNext()) {
+            Search search =  iterator.next();
+            search.searchDoc("hello world");
+        }
+    }
+}
